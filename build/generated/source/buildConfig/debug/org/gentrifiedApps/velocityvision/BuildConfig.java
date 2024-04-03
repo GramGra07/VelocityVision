f8b9f74c171380f92dc30e843f7e2392b2e8452f
Graden <101433010+GramGra07@users.noreply.github.com>
2024-04-02 21:15:12 -0600

    updates to 1.0.0

---
+++ build/generated/source/buildConfig/debug/org/gentrifiedApps/velocityvision/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.gentrifiedApps.velocityvision;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.gentrifiedApps.velocityvision";
+  public static final String BUILD_TYPE = "debug";
+}
